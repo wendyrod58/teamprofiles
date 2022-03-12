22e23e6dbcf5c939b68ae393f199b97e2f3ff371
Wendy Rodriguez <wendyrod58@gmail.com>
2022-03-12 08:42:53 -0800

    add Engineer questions

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 //Require all packages 
 const inquirer = require("inquirer");
+const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
-const mangerQuestions = [{
+const managerQuestions = [{
     name: "managerName",
     type: 'input',
     message: "Please enter Manager Name"
@@ -47,13 +48,35 @@ const internQuestions = [{
 }
 ]
 
+const EngineerQuestions = [{
+    name: "engineerName",
+    type: 'input',
+    message: "Please enter Engineer's Name"
+},
+{
+    name: "email",
+    type: 'input',
+    message: "Please enter Engineer's Email: "
+},
+{
+    name: "engineerID",
+    type: 'input',
+    message: "Please enter Engineer's ID"
+},
+{
+    name: "GitHubUserName",
+    type: 'input',
+    message: "Please enter Engineer's GitHubUserName: "
+}
+]
+
 let allTeamMembers = [];
 
 function init() {
     console.log("/n Welcome to team Generator /n ***************");
-    inquirer.prompt(mangerQuestions)
+    inquirer.prompt(managerQuestions)
         .then(answers => {
-            // console.log("manger Info", answers);
+            // console.log("manager Info", answers);
             //Convert into a Manager Class object 
             const sample = new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.officeNumber);
 
@@ -70,7 +93,7 @@ function init() {
 function options(){
     inquirer.prompt([{
         name:"myoptions", 
-        message: "would you like to adda member? ", 
+        message: "would you like to add a member? ", 
         choices:["Intern", "Engineer", "No , thank you"], 
         type:'list'
     }])
@@ -88,7 +111,7 @@ function options(){
 
 
 function buildHTMLPage(){
-    console.log("Generate TEmplate HTML"); 
+    console.log("Generate Template HTML"); 
     //fs.writefile 
 
     console.log("Bye!!!")
@@ -97,10 +120,25 @@ function buildHTMLPage(){
 function askInternQuestions() {
     inquirer.prompt(internQuestions)
         .then(answers => {
-            // console.log("manger Info", answers);
+            // console.log("manager Info", answers);
             //Convert into a Manager Class object 
             const sample = new Intern(answers.internName, answers.internID, answers.email, answers.school);
             //add it to the array 
+            allTeamMembers.push(sample);
+
+            //print all members 
+            console.log(allTeamMembers);
+
+            //ask the question 
+            options(); 
+
+        })
+};
+
+function askEngineerQuestions() {
+    inquirer.prompt(EngineerQuestions)
+        .then(answers => {
+            const sample = new Engineer(answers.engineerName, answers.engineerID, answers.email, answers.GitHubUserName);
             allTeamMembers.push(sample);
 
             //print all members 
